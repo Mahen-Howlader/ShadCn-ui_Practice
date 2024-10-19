48c1a48cd412d6825d052173987d710ba12314c1
devmahin <mahinhowlader123@gmail.com>
2024-10-20 00:12:52 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import { DaylogDemo } from './components/DaylogDemo'
+import AlertDialogs from './components/AlertDialogs'
+
+function App() {
+  return (
+    <div className='bg-gray-800 min-h-screen'>
+      <DaylogDemo></DaylogDemo>
+      <AlertDialogs></AlertDialogs>
+    </div>
+  )
+}
+
+export default App
