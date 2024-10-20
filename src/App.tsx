414ca2a7bf7b39e425a270ad27d586ee699af71b
devmahin <mahinhowlader123@gmail.com>
2024-10-20 23:22:56 +0530

    charts

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import { DaylogDemo } from './components/DaylogDemo'
 import AlertDialogs from './components/AlertDialogs'
 import AcordianDem from './components/AcordianDem'
+import PieCharts from './components/PieCharts'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <DaylogDemo></DaylogDemo>
       <AlertDialogs></AlertDialogs>
      <AcordianDem></AcordianDem>
+     <PieCharts></PieCharts>
     </div>
   )
 }
